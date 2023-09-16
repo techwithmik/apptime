4fc1dda30da0bbaee0e9141920fd9f63e9a6a9c9
techwithmik <mikisingh@gmail.com>
2023-09-16 11:40:08 -0400

    Updated tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
+import React from 'react'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div>
-            <h1>Welcome! You are ready to build 
-                a Next Js apps.</h1> 
+            <h1>Welcome! You are ready to build a few apps in Next Js.</h1> <br>Thanks for visiting TechWithMik</br>
         </div>
     </main>
   )
